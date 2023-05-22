fe327e3805af616223b6d7d8e3618a0c9810dfee
sarmhi <sammiejay813@gmail.com>
2023-05-22 10:02:58 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>Home</div>
+      <div>Services</div>
+      <div>Gallery</div>
+      <div>About Us</div>
+      <div>Contact</div>
+    </div>
+  );
+};
+
+export default Navbar;
